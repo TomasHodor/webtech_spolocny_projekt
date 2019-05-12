7aba011724c608abcbf84043206c7c650e20963c
Kalan <kalanin.peto@gmail.com>
2019-05-12 16:41:11 +0200

    Zakladna struktura
    Vytvorenie zakladnej struktury stranky

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+</head>
+
+<body>
+  <?php
+  if (isset($_GET['auth'])) {
+    echo 'Musite sa prihlasit';
+  }
+  ?>
+  <form action="index.php" method="post">
+    Login: <input name="login"><br>
+    Password: <input name="pass" type="password"><br>
+    <button type="submit">Odosli</button>
+  </form>
+
+</body>
+
+</html>

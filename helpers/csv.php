4dda52a270b3ea50b7fb83d897925fc4395c1ff9
Kalan <kalanin.peto@gmail.com>
2019-05-12 18:39:31 +0200

    praca s csv
    Prevod obsahu csv suboru na strukturovane pole udajov

--- helpers/csv.php
+++ helpers/csv.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Sluzba prevedie obsah csv suboru na strukturovane pole
- * @author Peter Kalanin
- * 
- * @param String $csvText Obsah csv suboru
- * @param String $delim Oddelovac dat
- * 
- */
+
 function csvToTable($csvText, $delim = ";")
 {
   $table = array();

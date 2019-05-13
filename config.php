9730ef844f139a1e70f18b008d7c5fc58e1fc2fe
Kalan <kalanin.peto@gmail.com>
2019-05-13 11:18:26 +0200

    sql s users tabulkou, config.php
    Pridal som subor sql, ktorý importuje do vasej db. V config.php si nastavte udaje do vasej db.

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
-session_start();
+define("servername", "localhost");
+define("username", "vas username"); // Sem doplnte vas username do DB
+define("password", "vase heslo"); // Sem doplnte vase heslo do DB
+define("database", "spolocny_projekt");
+
+if (!session_id()) {
+  session_start();
+}
 
 $ldapServer = "ldap.stuba.sk";

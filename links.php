257bbcee6bd5f9789256704109faf66fb2d4601d
Kalan <kalanin.peto@gmail.com>
2019-05-13 10:41:43 +0200

    linky, include vo vsetkych suboroch
    Vytvoril som subor links.php, ktory includuju vsetky php subory vyuzivajuce boostrap, jquery a fontawesome

---
+++ links.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>

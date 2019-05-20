5f630a46480efd923c40bbe5df754ae3a6410cc4
kybelGitu <albrechtjasovsky@gmail.com>
2019-05-20 09:54:12 +0200

    "vlozenie do DB"

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 define("servername", "localhost");
-define("username", "vas username"); // Sem doplnte vas username do DB
-define("password", "vase heslo"); // Sem doplnte vase heslo do DB
-define("database", "spolocny_projekt");
+define("username", "root"); // Sem doplnte vas username do DB
+define("password", ""); // Sem doplnte vase heslo do DB
+define("database", "zav_zad");
+
 
 if (!session_id()) {
   session_start();
 }
 
 $ldapServer = "ldap.stuba.sk";
+
